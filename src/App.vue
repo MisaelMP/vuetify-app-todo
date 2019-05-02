22e863db2b6371bd8a87560f9fc4b16146867d96
Misael  M <phatarino@gmail.com>
2019-05-02 21:56:14 +1000

    added postcss plugins and example

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,58 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+<v-app>
+    <v-toolbar app dark>
+        <v-toolbar-title class="headline text-uppercase">
+            <span>Vuetify Website</span>
+        </v-toolbar-title>
+    </v-toolbar>
+    <v-spacer></v-spacer>
+    <router-view></router-view>
+    v
+</v-app>
 </template>
+
+<script>
+export default {}
+</script>
+
+<style lang="postcss" scoped>
+</style>
+
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
 }
+
 #nav {
-  padding: 30px;
+    padding: 30px;
 }
 
 #nav a {
-  font-weight: bold;
-  color: #2c3e50;
+    font-weight: bold;
+    color: #2c3e50;
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+    color: #42b983;
 }
 </style>
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+    name: 'App',
+    components: {
+        HelloWorld
+    },
+    data() {
+        return {
+            //
+        }
+    }
+}
+</script>
