ba2db553efb0ea57e11179a3a29c31e0b6be0858
Misael  M <phatarino@gmail.com>
2019-05-12 17:51:04 +1000

    added date picker and validation to form

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -1,46 +1,54 @@
 <template>
-  <v-dialog
-    max-width="600px"
-    transition="dialog-transition"
-  >
+<v-dialog max-width="600px" transition="dialog-transition">
     <v-btn slot="activator" depressed color="#f6db4e" class="dark--text">Add new project</v-btn>
     <v-card>
-        <v-card-title >
-          <h2>Add a New Project</h2>
+        <v-card-title>
+            <h2>Add a New Project</h2>
         </v-card-title>
         <v-card-text>
-            <v-form class="px-3" @submit="submit">
-                <v-text-field
-                  name="title"
-                  label="title"
-                  id="title"
-                  v-model="title"
-                  prepend-icon="folder"
-                ></v-text-field>
-                <v-textarea
-                label="Information"
-                v-model="content"
-                prepend-icon="edit">
+            <v-form class="px-3" @submit="submit" ref="form">
+                <v-text-field name="title" label="title" id="title" v-model="title" prepend-icon="folder" :rules="inputRules"></v-text-field>
+                <v-textarea label="Information" v-model="content" prepend-icon="edit" :rules="inputRules">
                 </v-textarea>
+                <v-menu>
+                    <v-text-field :value="formattedDate" slot="activator" label="Due Date" prepend-icon="date_range" :rules="inputRules"></v-text-field>
+                    <v-date-picker v-model="due" color="indigo" :reactive="true"></v-date-picker>
+                </v-menu>
+                <v-spacer></v-spacer>
                 <v-btn depressed color="#f6db4e" class="dark--text mx-0 mt-3 text-uppercase" type="submit">Add Project</v-btn>
             </v-form>
         </v-card-text>
     </v-card>
-  </v-dialog>
+</v-dialog>
 </template>
 
 <script>
+import format from 'date-fns/format'
+
 export default {
     data() {
         return {
             title: '',
-            content: ''
+            content: '',
+            due: null,
+            errors: [],
+            inputRules: [
+                v => v.length >= 3 || 'Minimum length is 3 characters'
+            ]
         }
     },
     methods: {
         submit(e) {
-            console.log(this.title, this.content);
             e.preventDefault();
+            if (this.$refs.form.validate()) {
+                console.log(this.title, this.content);
+
+            }
+        }
+    },
+    computed: {
+        formattedDate() {
+            return this.due ? format(this.due, 'Do MMM YYYY') : ''
         }
     }
 }
